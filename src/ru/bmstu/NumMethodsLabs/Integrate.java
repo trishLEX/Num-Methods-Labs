70d4fba910aae04fd6b205f5f7ef8f46e81073a0
trishLEX <alexeytr5@gmail.com>
2018-04-08 13:55:53 +0300

    Добавлена остановка счёта по Рунге.

--- src/ru/bmstu/NumMethodsLabs/Integrate.java
+++ src/ru/bmstu/NumMethodsLabs/Integrate.java
@@ -1,30 +1,49 @@
 package ru.bmstu.NumMethodsLabs;
 
 public class Integrate {
-    private static final int N = 10;
+    private static final int N = 2;
     private static final int A = 0;
     private static final int B = 1;
+    private static final double E = 0.001;
 
     public static void main(String[] args) {
-        double[] x = new double[N + 1];
-        double[] y = new double[N + 1];
-        double h = (double) (B - A) / N;
-
-        for (int i = 0; i <= N; i++) {
-            x[i] = (double) i / N;
-            y[i] = Math.exp(x[i]);
-        }
-
-        double IbyCentralRects = centralRects(x, h);
-        double IbyTrapeze = trapeze(x, h);
         double ideal = Math.E - 1;
 
-        System.out.println("By central rects I* = " + IbyCentralRects + " error = " + Math.abs(ideal - IbyCentralRects));
-        System.out.println("By trapeze I* = " + IbyTrapeze + " error = " + Math.abs(ideal - IbyTrapeze));
+        double IbyCentralRects  = centralRects(N);
+        double IbyCentralRects2 = centralRects(N * 2);
+        double R = Math.abs(IbyCentralRects2 - IbyCentralRects) / 3;
+        int n;
+        for (n = 4; R > E; n *= 2) {
+            IbyCentralRects  = centralRects(n);
+            IbyCentralRects2 = centralRects(n * 2);
+            R = Math.abs(IbyCentralRects2 - IbyCentralRects) / 3;
+        }
+
+        System.out.println("By central rects I* = " + IbyCentralRects2 + " error = " + Math.abs(ideal - IbyCentralRects2)
+                + " N = " + n + " I* + R = " + (IbyCentralRects2 + R) + " errorR = " + Math.abs(IbyCentralRects2 + R - ideal));
+
+        double IbyTrapeze  = trapeze(N);
+        double IbyTrapeze2 = trapeze(N * 2);
+        R = Math.abs(IbyTrapeze2 - IbyTrapeze) / 3;
+        for (n = 4; R > E; n *= 2) {
+            IbyTrapeze  = trapeze(n);
+            IbyTrapeze2 = trapeze(n * 2);
+            R = Math.abs(IbyTrapeze2 - IbyTrapeze) / 3;
+        }
+
+        System.out.println("By trapeze I* = " + IbyTrapeze2 + " error = " + Math.abs(ideal - IbyTrapeze2)
+                + " N = " + n + " I* + R = " + (IbyTrapeze2 + R) + " errorR = " + Math.abs(IbyTrapeze + R - ideal));
+
         System.out.println("Ideal I = " + ideal);
     }
 
-    private static double centralRects(double[] x, double h) {
+    private static double centralRects(int N) {
+        double[] x = new double[N + 1];
+        double h = (double) (B - A) / N;
+
+        for (int i = 0; i <= N; i++)
+            x[i] = (double) i / N;
+
         double I = 0;
 
         for (int i = 1; i <= N; i++) {
@@ -34,7 +53,13 @@ public class Integrate {
         return I;
     }
 
-    private static double trapeze(double[] x, double h) {
+    private static double trapeze(int N) {
+        double[] x = new double[N + 1];
+        double h = (double) (B - A) / N;
+
+        for (int i = 0; i <= N; i++)
+            x[i] = (double) i / N;
+
         double I = h * (Math.exp(A) + Math.exp(B)) / 2;
 
         for (int i = 1; i < N; i++) {
