0abc7f16eacee411f6675d8095a0a0c06efed6d8
trishLEX <alexeytr5@gmail.com>
2018-04-26 12:04:16 +0300

    Лабораторная работа №7.

--- src/ru/bmstu/NumMethodsLabs/ExtrFind.java
+++ src/ru/bmstu/NumMethodsLabs/ExtrFind.java
@@ -3,31 +3,31 @@ package ru.bmstu.NumMethodsLabs;
 public class ExtrFind {
     private static final double E = 0.00001;
 
-    private double f(double x, double y) {
-        return x * x + 10 * (y - Math.sin(x)) * (y - Math.sin(x));
+    private static double f(double[] x) {
+        return x[0] * x[0] + 10 * (x[1] - Math.sin(x[0])) * (x[1] - Math.sin(x[0]));
     }
 
-    private double derivFx(double x, double y) {
-        return 2 * x - 20 * (y - Math.sin(x)) * Math.cos(x);
+    private static double derivFx(double[] x) {
+        return 2 * x[0] - 20 * (x[1] - Math.sin(x[0])) * Math.cos(x[0]);
     }
 
-    private double derivFy(double x, double y) {
-        return 20 * (y - Math.sin(x));
+    private static double derivFy(double[] x) {
+        return 20 * (x[1] - Math.sin(x[0]));
     }
 
-    private double derivFxx(double x, double y) {
-        return 2 - 20 * (- Math.cos(x) * Math.cos(x) - Math.sin(x) * (y - Math.sin(x)));
+    private static double derivFxx(double[] x) {
+        return 2 - 20 * (- Math.cos(x[0]) * Math.cos(x[0]) - Math.sin(x[0]) * (x[1] - Math.sin(x[0])));
     }
 
-    private double derivFxy(double x) {
-        return - 20 * Math.cos(x);
+    private static double derivFxy(double[] x) {
+        return - 20 * Math.cos(x[0]);
     }
 
-    private double derivFyy() {
+    private static double derivFyy() {
         return 20;
     }
 
-    private double[][] invertMatrix(double[][] A) {
+    private static double[][] invertMatrix(double[][] A) {
         double[][] res = new double[2][2];
         res[0][0] = A[1][1];
         res[0][1] = - A[0][1];
@@ -43,16 +43,35 @@ public class ExtrFind {
         return res;
     }
 
+    private static double[] mul(double[][] A, double[] b) {
+        double[] res = new double[2];
+        res[0] = A[0][0] * b[0] + A[0][1] * b[1];
+        res[1] = A[1][0] * b[0] + A[1][1] * b[1];
+        return res;
+    }
+
+    private static double[] sub(double[] a, double[] b) {
+        return new double[]{a[0] - b[0], a[1] - b[1]};
+    }
+
     public static void main(String[] args) {
-        double[] x = {0.572, 0.610};
-        double[] g = {0.289, 3.171};
-        double[][] G = {{10.507, -10.806},
-                        {10.806, 20}};
-        double[] alpha = {1, 1};
-        double[] p = {0.428, -0.390};
+        double[] xPrev = {1, 1};
+        double[] g = {derivFx(xPrev), derivFy(xPrev)};
+        double[][] G = {{derivFxx(xPrev), derivFxy(xPrev)},
+                        {derivFxy(xPrev), derivFyy()}};
+        double[] xNext = sub(xPrev, mul(invertMatrix(G), g));
 
-        while (Math.max(x[0], x[1]) > E) {
+        while (Math.max(Math.abs(xNext[0] - xPrev[0]), Math.abs(xNext[1] - xPrev[0])) > E) {
+            xPrev = xNext.clone();
+            xNext = sub(xPrev, mul(invertMatrix(G), g));
+            G[0][0] = derivFxx(xNext);
+            G[1][0] = G[0][1] = derivFxy(xNext);
+            G[1][1] = derivFyy();
 
+            g[0] = derivFx(xNext);
+            g[1] = derivFy(xNext);
         }
+
+        System.out.println("RESULT: (" + xNext[0] + ", " + xNext[1] + ")");
     }
 }
