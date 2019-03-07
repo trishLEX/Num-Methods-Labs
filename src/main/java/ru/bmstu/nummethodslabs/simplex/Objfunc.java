fe832bf3bd9e20c48af06735e0d3bd03f7417c17
trishLEX <alexeytr5@gmail.com>
2019-03-07 11:19:36 +0300

    Tab to 4 spaces

--- src/main/java/ru/bmstu/nummethodslabs/simplex/Objfunc.java
+++ src/main/java/ru/bmstu/nummethodslabs/simplex/Objfunc.java
@@ -2,14 +2,14 @@ package ru.bmstu.nummethodslabs.simplex;
 
 public class Objfunc
 {
-	public double evalObjfun(double[] x){
-		double sum = 80;
-		double a = 30;
-		double b = 2;
-		for (int i = 0; i < 3; i++) {
-			sum += a * Math.pow(x[i] * x[i] - x[i+1], 2) + b * Math.pow(x[i] - 1, 2);
-		}
+    public double evalObjfun(double[] x){
+        double sum = 80;
+        double a = 30;
+        double b = 2;
+        for (int i = 0; i < 3; i++) {
+            sum += a * Math.pow(x[i] * x[i] - x[i+1], 2) + b * Math.pow(x[i] - 1, 2);
+        }
 
-		return sum;
-	}
+        return sum;
+    }
 }
--- src/main/java/ru/bmstu/nummethodslabs/simplex/Testfun.java
+++ src/main/java/ru/bmstu/nummethodslabs/simplex/Testfun.java
@@ -4,18 +4,18 @@ import java.io.*;
 
 public class Testfun
 {
-	public static void main(String[] args) throws IOException
-	{
-		double[] start = {0, 0, 0, 0};
-		int dim = 4;
-		double eps = 0.001;
-		double scale = 0.01;
+    public static void main(String[] args) throws IOException
+    {
+        double[] start = {0, 0, 0, 0};
+        int dim = 4;
+        double eps = 0.001;
+        double scale = 0.01;
 
-		NMSimplex simplex = new NMSimplex(start, dim, eps, scale);
+        NMSimplex simplex = new NMSimplex(start, dim, eps, scale);
 
-		for (int i = 0; i < dim; i++) {
-			System.out.format("%f\n", start[i]);
-		}
-	}
+        for (int i = 0; i < dim; i++) {
+            System.out.format("%f\n", start[i]);
+        }
+    }
 
 }
